52f3c259e08f4e0d8547ef149022b037b7b95d97
anilb\Anil Basibuyuk <anil.basibuyuk@gybegames.com>
2023-05-20 23:57:03 +0300

    feat(RouteFollower): add set linear speed method

--- RouteDrifter/Follower/RouteFollower.cs
+++ RouteDrifter/Follower/RouteFollower.cs
@@ -56,6 +56,10 @@ namespace RouteDrifter.Follower
         // }
         
         #endregion
-        
+
+        public void SetLinearSpeed(float speed)
+        {
+            _LinearSpeed = speed;
+        }
     }
 }
